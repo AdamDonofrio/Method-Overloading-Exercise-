6f885ae801d865714b53e23594db315ceccdcc89
unknown <donofrio.8@wright.edu>
2023-04-13 15:13:48 -0400

    first commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,46 @@
 {
     public class Program
     {
+        public static int Add(int a, int b) 
+        { 
+            return a + b;
+        }
+        public static double Add(double a, double b) 
+        {  
+            return a + b; 
+        }
+        public static string Add(int a, int b, bool c)
+        {
+            if (c == true)
+            {
+                if (a + b == 1)
+                {
+                    return $"{a + b} dollar";
+                }
+                else
+                {
+                    return $"{a + b} dollars";
+                }
+                
+            }
+            else
+            {
+                return "False";
+            }
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int intA = 1;
+            int intB = 2;
+            double doubleA = 1.5;
+            double doubleB = 2.5;
+            bool boolA = true;
+
+            Console.WriteLine($"Add {intA} and {intB} is {Add(intA, intB)}");
+            Console.WriteLine($"Add {doubleA} and {doubleB} is {Add(doubleA, doubleB)}");
+            Console.WriteLine($"Add {intA} and {intB} with the boolean being {boolA} is {Add(intA, intB, boolA)}");
         }
     }
 }
